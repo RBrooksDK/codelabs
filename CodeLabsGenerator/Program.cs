eb129b8937a0771535763c389f3872b1cfb79608
RBrooksDK <richardbrooks.dk@gmail.com>
2022-09-04 00:26:55 +0200

    small error

--- CodeLabsGenerator/Program.cs
+++ CodeLabsGenerator/Program.cs
@@ -9,7 +9,7 @@ namespace CodeLabsGenerator
 
         static void Main(string[] args)
         {
-            GenerateOne("DvdRentalExercises");
+            GenerateOne("WebshopExercises");
             // GenerateMany();
         }
 
